612563e0feba3e004655fb364e26f57529c33cd5
Edward Diener <eldiener@tropicsoft.com>
2016-11-07 15:33:55 -0500

    Include math.h first to avoid Python _hypot redefinition bug

--- test/python_test.cpp
+++ test/python_test.cpp
@@ -2,6 +2,7 @@
 // subject to the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+#include <math.h>
 #include <boost/python.hpp>
 #include <boost/parameter/preprocessor.hpp>
 #include <boost/parameter/keyword.hpp>
