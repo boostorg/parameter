1a1f39ee06be95fcea21e560a8ee68177b78a77d
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-04-06 22:39:24 -0400

    BOOST_PARAMETER_NAME vs MSVC
    Fix attempt based on error messages emitted by Appveyor.

--- include/boost/parameter/keyword_fwd.hpp
+++ include/boost/parameter/keyword_fwd.hpp
@@ -6,32 +6,14 @@
 #ifndef BOOST_PARAMETER_KEYWORD_FWD_HPP
 #define BOOST_PARAMETER_KEYWORD_FWD_HPP
 
-#include <boost/config.hpp>
-
 namespace boost { namespace parameter {
 
-#if defined BOOST_MSVC
-    struct in;
-    struct out;
-    typedef boost::parameter::out in_out;
-    struct forward;
-    struct consume;
-    typedef boost::parameter::consume move_from;
-
-    typedef boost::parameter::in in_reference;
-    typedef boost::parameter::out out_reference;
-    typedef boost::parameter::in_out in_out_reference;
-    typedef boost::parameter::forward forward_reference;
-    typedef boost::parameter::consume consume_reference;
-    typedef boost::parameter::move_from move_from_reference;
-#else
     struct in_reference;
     struct out_reference;
     typedef boost::parameter::out_reference in_out_reference;
     struct forward_reference;
     struct consume_reference;
     typedef boost::parameter::consume_reference move_from_reference;
-#endif // BOOST_MSVC
 
     template <class Tag>
     struct keyword;
