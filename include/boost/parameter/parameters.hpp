5fb1ef2871d8f3f855a1a91cf87b764d11e0db5a
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-10-31 00:41:49 -0400

    Fix boost/parameter/parameters.hpp
    Change erroneous #include statement from <boost/fusion/container/vector/vector_fwd.hpp> to <boost/fusion/container/list/list_fwd.hpp>.

--- include/boost/parameter/parameters.hpp
+++ include/boost/parameter/parameters.hpp
@@ -67,13 +67,13 @@ namespace boost { namespace parameter { namespace aux {
 #include <boost/parameter/aux_/pack/make_parameter_spec_items.hpp>
 #include <boost/parameter/aux_/pack/tag_keyword_arg.hpp>
 #include <boost/parameter/aux_/pack/tag_template_keyword_arg.hpp>
-#include <boost/fusion/container/vector/vector_fwd.hpp>
-#include <boost/fusion/container/deque/deque_fwd.hpp>
 #include <boost/mpl/bool.hpp>
 #include <boost/mpl/pair.hpp>
 #include <boost/mpl/identity.hpp>
 
 #if !defined(BOOST_PARAMETER_VARIADIC_MPL_SEQUENCE)
+#include <boost/fusion/container/list/list_fwd.hpp>
+
 // Newer versions of MSVC fail on the evaluate_category and
 // preprocessor_eval_category test programs when parameters uses
 // boost::fusion::list.
@@ -84,14 +84,18 @@ namespace boost { namespace parameter { namespace aux {
 #include <boost/fusion/container/list.hpp>
 #include <boost/fusion/mpl.hpp>
 #define BOOST_PARAMETER_VARIADIC_MPL_SEQUENCE ::boost::fusion::list
-#elif defined(BOOST_FUSION_HAS_VARIADIC_DEQUE)
+#else
+#include <boost/fusion/container/deque/deque_fwd.hpp>
+
+#if defined(BOOST_FUSION_HAS_VARIADIC_DEQUE)
 #include <boost/fusion/container/deque.hpp>
 #include <boost/fusion/mpl.hpp>
 #define BOOST_PARAMETER_VARIADIC_MPL_SEQUENCE ::boost::fusion::deque
 #else
 #include <boost/mpl/vector.hpp>
 #define BOOST_PARAMETER_VARIADIC_MPL_SEQUENCE ::boost::mpl::vector
-#endif
+#endif  // BOOST_FUSION_HAS_VARIADIC_DEQUE
+#endif  // BOOST_FUSION_HAS_VARIADIC_LIST
 #endif  // BOOST_PARAMETER_VARIADIC_MPL_SEQUENCE
 
 namespace boost { namespace parameter {
