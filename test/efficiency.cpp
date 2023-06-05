02e1c566f7baf69c9ae33b9ec7a40c20fe054de9
Andrey Semashev <andrey.semashev@gmail.com>
2023-06-05 16:03:18 +0300

    Copy boost::timer to tests since the original is deprecated and giving errors.
    We're not using Boost.Timer v2 components to avoid linking tests with its
    separately compiled binary. The legacy boost::timer is fine for the test
    purposes.

--- test/efficiency.cpp
+++ test/efficiency.cpp
@@ -5,8 +5,8 @@
 
 #include <boost/parameter/name.hpp>
 #include <boost/config/workaround.hpp>
-#include <boost/timer.hpp>
 #include <iostream>
+#include "timer.hpp"
 
 namespace test {
 
@@ -147,7 +147,7 @@ namespace test {
         test::hammer<Accumulator>(x, repeats);
 
         // Now start a timer.
-        boost::timer time;
+        test::timer time;
         test::hammer<Accumulator>(x, repeats);  // This time, we'll measure.
         return time.elapsed();
     }
@@ -163,7 +163,7 @@ int main()
     {
         repeats *= 10;
 
-        boost::timer time;
+        test::timer time;
 
         test::hammer<test::plain_weight_running_total<double> >(.1, repeats);
         test::hammer<test::named_param_weight_running_total<double> >(
@@ -172,7 +172,7 @@ int main()
 
         measured = time.elapsed();
     }
-    
+
     std::cout
         << "plain time:           "
         << test::measure<test::plain_weight_running_total<double> >(
